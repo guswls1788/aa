c823d8b66bdbcc3e73462eb73090a06c70f8d5a0
[guswls1788] <[guswls1788@naver.com]>
2022-02-10 21:05:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Test from './Test';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+        app
+        <BrowserRouter>
+         <Routes>
+          <Route exact path="/test" element={<Test/>} />
+        </Routes>
+         </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react/cjs/react.production.min';
+
+class Test extends Component{
+  render(){
+    return(
+      <>
+      test</>
+    )
+  }
+}
+
+export default Test;
